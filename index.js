ae3add70301d2e734fc22c1a1d9338b20aa5c4b0
UMESHRAUT <rautumesh34@gmail.com>
2020-09-29 10:22:15 +0530

    make it better

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const mailApi="SG.n64TArNoRf-V0iS1zrNNZA.g8cgnUTIqMqlNjBh1nlTzQkF_H6eydL73YXvIb2
 
 app.use(express.json())
 
-mongoose.connect("mongodb://localhost/reminder",()=>{
+mongoose.connect("mongodb+srv://Umesh123:Umesh123@cluster0.moi8u.mongodb.net/reminder?retryWrites=true&w=majority",()=>{
     console.log("mongo db connected");
 });
 
